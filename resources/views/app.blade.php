69cbb0c02b79899ac452eca70907f594b054900d
André Da Silva <Andre.Aquilau@outlook.com>
2021-03-24 04:05:38 -0400

    'projeto-pronto'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,19 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
+        <!--Link do CSS-->
+        <link rel="shortcut icon" href="{{ asset('icone.png')}}" type="image/x-png">
+        <title>Agenda</title>
+        <script src="https://kit.fontawesome.com/3d5793b601.js" crossorigin="anonymous"></script>
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased" id="app-root">
       @inertia
-    </body>
+
+   </body>
 </html>
